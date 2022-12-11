c6255e012a9fb9c76e84c644fd69b8cecb491b98
djidji3 <djidji@freemail.hu>
2022-12-11 13:06:47 +0100

    su

---
+++ su-sudo.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# modszerek identitas valtasra
+# login,su, sudo
+# su parancs egy masik userre valo valtast jelent egy uj shellben, vagy egyetlen parancs vegrehajtasat
+# a su bekeri az adott user jelszavat
+su [-[l]] [user]
+    # -l :login shell kap a megadott user
+su -
+    # root userre fog valtani
+
+
+# sudo program segitsegevel a root be tudja allitani a /etc/sudoers fajlban hogy milyen programot,
+# milyen user hajthat vegre
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
