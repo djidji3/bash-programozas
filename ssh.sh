419d25d59e968d1133b003422bd8e743adc4e393
Djidji3 <djidji@freemail.hu>
2022-12-16 18:39:12 +0100

    ssh javitas

--- ssh.sh
+++ ssh.sh
@@ -9,30 +9,3 @@
 # a tavoli grafikus programot ami az xload, inditsuk el
 # xload
 # igy a tavoli xload program elindul a tavoli gepen, es megjelenitjuk azt a helyi gepunkon
-
-#####################
-archiving-backup.sh
-#!/bin/bash
-
-# gzip,bzip fajl tomorites tomorites - ha mappakat is kell tomoriteni akkor eloszor a tar programot hasznaljuk
-gzip file.txt
-bzip2 file.txt
-
-# gzip,bzip kitomorites 
-gunzip file.txt.gz
-gzip -d file.txt.gz
-bzip2 -d file.txt.gz
-
-# tomoritett fajl listazasa
-gunzip -c file.txt.gz | less
-zcat file.txt.gz | less
-
-# pipe segitsegevel
-ls -lha /etc | gzip > etc.gz
-
-# gzip opciok
--c tartsa meg az eredetifajl is a kicsomagolas utan, vagy listazza ki a tomoritett fajlt
--f force vagyis akkor is megcsinalja ha mar letezik a fajl
--l listazza a tomorites merteket is
--t ellenorzi a tomoritett fajl integritasat
--number tomorites mertekenek megadasa
